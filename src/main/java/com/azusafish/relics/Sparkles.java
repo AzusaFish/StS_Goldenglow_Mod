60662f5c4aafef6553cff101325fada9617720c8
AzusaFish <legends_fish@outlook.com>
2026-02-07 19:22:50 +0800

    V0.0.1

--- src/main/java/com/azusafish/relics/Sparkles.java
+++ src/main/java/com/azusafish/relics/Sparkles.java
@@ -2,34 +2,22 @@ package com.azusafish.relics;
 
 import basemod.abstracts.CustomRelic;
 import com.azusafish.orbs.SparkOrb;
-import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.actions.defect.ChannelAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.orbs.AbstractOrb;
 
 public class Sparkles extends CustomRelic {
-    // 遗物ID
     public static final String ID = "AzusaFish:Sparkles";
-    // 图片路径 (你需要准备这张图)
+    // 只保留这一张存在的图片
     private static final String IMG = "images/relics/Sparkles.png";
-    // 轮廓图 (可选，没有就用同一个)
-    private static final String OUTLINE = "images/relics/Sparkles.png";
 
-    public Sparkles() {
-        super(ID, ImageMaster.loadImage(IMG), ImageMaster.loadImage(OUTLINE), RelicTier.STARTER, LandingSound.MAGICAL);
-    }
-
-    // 如果你没有做 outline 图片，可以用这个简单的构造函数：
-    /*
     public Sparkles() {
         super(ID, ImageMaster.loadImage(IMG), RelicTier.STARTER, LandingSound.MAGICAL);
     }
-    */
 
     @Override
     public void atTurnStart() {
-        // 检查是否有 Spark
         boolean hasSpark = false;
         for (AbstractOrb orb : AbstractDungeon.player.orbs) {
             if (orb instanceof SparkOrb) {
@@ -38,9 +26,8 @@ public class Sparkles extends CustomRelic {
             }
         }
 
-        // 如果没有，生成一个
         if (!hasSpark) {
-            flash(); // 遗物闪烁一下
+            flash();
             addToBot(new ChannelAction(new SparkOrb()));
         }
     }
